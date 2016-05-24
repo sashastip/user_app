70d46db7fe664d0662d04567726c967f6bf53cda
sashastip <saschastipanenko@gmail.com>
2016-05-24 17:07:26 +0300

    added custom search method

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,19 @@ require 'Elasticsearch/model'
 class Article < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title^10', 'text']
+          }
+        }
+      }
+    )
+  end
 end
 
 Article.import
