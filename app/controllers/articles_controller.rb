7c826ffead0dc568f0350ef82c56b5ee0a4050d2
sashastip <saschastipanenko@gmail.com>
2016-05-24 15:30:34 +0300

    add article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,27 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Articles.all
+  end
+
+  def show
+    @article = Article.find[:id]
+  end
+
+  def new
+  end
+  
+  def create
+    @article = Article.new
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit :title, :text
+    end
 end
