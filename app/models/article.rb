8be7748ea6f0d48feeb7c9b8cd7e6fdfcdf9d196
sashastip <saschastipanenko@gmail.com>
2016-05-25 10:34:10 +0300

    chenged Article search

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,14 +3,5 @@ require 'elasticsearch/model'
 class Article < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
-
-  validates :title, :text, presence: true
-
-  def as_indexed_json(options={})
-    self.as_json(
-      only: [:id, :title, :text]
-      )
-  end
 end
-
 Article.import
