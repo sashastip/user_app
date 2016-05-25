70d5953f6ecf015e092767e40bcf2f08025e6cf9
sashastip <saschastipanenko@gmail.com>
2016-05-25 12:27:59 +0300

    fix issues

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,9 @@
 class SearchController < ApplicationController
   def search
-    @articles = Article.search params[:q]
-    
-    respond_to do |format|
-      if params[:q].nil?
-        @article = []
-        format.html { @article }
-        format.js {}
-      else
-        format.html { @article }
-        format.js {}
-      end
+    if params[:q].nil?
+      @articles = []
+    else
+      @articles = Article.search params[:q]
     end
   end
 end
